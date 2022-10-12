c3ab784a1021d050cf59e5d2c7661f80858a87bb
Thiago Vinícius <thiagovao2000@hotmail.com>
2022-10-12 09:13:30 -0300

    Initial commit

---
+++ src/test/java/com/pds/sgg/SistemaDeGestaoDeSaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pds.sgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestaoDeSaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
